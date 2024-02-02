e920ab96c590b312e25443b90cd5c04a72e8c77a
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-02-02 18:19:46 +0300

    template literals example

---
+++ Template literals/apps.js
@@ -0,0 +1,13 @@
+//
+
+const a=10
+const b=100
+const c=1000
+console.log("hasan "+a+"  sabbah "+b+"  alamud "+c)
+
+console.log(`hasan ${a}
+             sabbah ${b}
+             alamud ${c}`)
+
+//hocam gördüğün üzere d,namik değerleri eklemek için + kullnamaktan 
+//daha konforlu. ayrıca fark etmişsindir enter ları boşluşarı falann hepsini algılıyor
