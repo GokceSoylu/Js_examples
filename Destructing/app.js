53815d23b177ed570a0cd8b9c4b8eb6e4c5cb376
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-02-02 06:04:59 +0300

    destructing with object

--- Destructing/app.js
+++ Destructing/app.js
@@ -18,4 +18,18 @@ const calculater=(x,y)=> //gördüğün üzre ; gerek yok bu destructing ile ilg
 }
 
 let [a,b,c,d]=calculater(11,2);  
-console.log(a,b,c,d); //yani hocam anlıyacağın burada abcd dizi değil bir dizinin elemaları forech vb olmadan kolaylıkla atmak için kullılan sytaxs
+console.log(a,b,c,d); //yani hocam anlıyacağın burada abcd dizi değil bir dizinin elemaları forech vb olmadan kolaylıkla atmak için kullılan sytax
+
+//birde obje ile yapmayı deneyilm
+//obje de dustructing yapmanın iki püf noktası var bir kullalınam key aynı oklmalı ki yakalayabilsin çünkü array olduğunda zatern sırayla index
+//değerine göre aatadığı için sorun yok am aobje de öyle değil neyin karşılığı ne bilebilmek için key kullanılmalı. hmm eğer değişken adı olarak keyi
+//değl farklı bir değişken kullanmak istesekte key:new_valueName yazarız hadi bakalım :))
+
+let student ={
+    name:"necmiye",
+    surname:"soylu",
+    age :23,
+    dep:"cmpe"
+};
+let {name,surname,age,dep:bolum}=student;
+console.log(name,surname,age,bolum);
